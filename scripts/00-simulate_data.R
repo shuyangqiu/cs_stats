be6b97f858d48e24de484121e125b46f979637e9
shuyangqiu <shuyangqiu@outlook.com>
2024-04-18 15:40:46 -0400

    simulate

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,19 +1,43 @@
 #### Preamble ####
-# Purpose: Simulates... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Simulates games played between players and summary statistics
+# for each player.
+# Author: Shuyang Qiu
+# Date: 17 April 2024
+# Contact: shuyang.qiu@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: None
 
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
 
 #### Simulate data ####
-# [...ADD CODE HERE...]
+set.seed(111)
 
+num_players <- 100
+num_games <- 1000
 
+# Simulate games played
+games <- tibble(
+  winner = sample(0:1, size = num_games, replace = TRUE),
+  player1 = sample(0:num_players, size = num_games, replace = TRUE),
+  player2 = sample(0:num_players, size = num_games, replace = TRUE)
+)
 
+# Simulate player stats
+player_stats <- tibble(
+  id = 1:100,
+  kills = sample(1000:10000, size = num_players, replace = TRUE),
+  deaths = sample(1000:10000, size = num_players, replace = TRUE)
+)
+
+#### Validate data ####
+games$winner == 0 | games$winner == 1
+games$player1 > 0 & games$player1 <= 100
+games$player2 > 0 & games$player2 <= 100
+
+player_stats$id > 0 & player_stats$id <= 100
+player_stats$wins > 0
+player_stats$loss > 0
+player_stats$kills > 0
+player_stats$deaths > 0
